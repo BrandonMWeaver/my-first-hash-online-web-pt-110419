65d824ba1ef5b055098337323f8f60e668fd5c6d
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 14:17:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,17 +15,8 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = { "whale bone corsets" => 5,
-                        "porcelain vases" => 2,
-                        "oil paintings" => 3 }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
